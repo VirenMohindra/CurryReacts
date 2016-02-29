8f9edb4a61c68ea97c035cfe1bcccc2ebfb67f4e
Viren Mohindra <viren.mohindra@gmail.com>
2016-02-29 14:44:47 -0500

    created popup.html and set it to launch on installation
    - popup.html with various stylesheets and and javascript
    - added google analytics (removed from GitHub for safety reasons)
    - rewrote manifest.json to launch popup.html on command

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function (object) {chrome.tabs.create({url: "popup.html"}, function (tab) {console.log("New tab launched with popup.html");});});
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'XXXXXXX']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
